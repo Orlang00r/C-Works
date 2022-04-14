e3282a50addf2e59783a91a79825a79c5f13afbf
Orlang00r <Orlangur00r@yandex.ru>
2022-04-14 16:44:57 +0400

    masha

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ввидите имя пользователя: ");
+var username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
