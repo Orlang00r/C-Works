8b1d45eaf5b67d0d0db064bce15d6e03878f3c11
Orlang00r <Orlangur00r@yandex.ru>
2022-04-14 15:30:15 +0400

    Rand

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,5 +1,7 @@
-﻿int NumberA = new Random().Next(1,10);
-int NumberB = new Random().Next(1,10);
-int result = NumberA+NumberB; 
+﻿int numberA = new Random().Next (1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next (1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
- 
+
