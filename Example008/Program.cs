af7fd1a5273ae7f4d2840b215c2938979fc79f84
Orlang00r <Orlangur00r@yandex.ru>
2022-04-21 10:32:15 +0400

    method+

---
+++ Example008/Program.cs
@@ -0,0 +1,22 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int res = arg1;
+    if (arg2 > res) res = arg2;
+    if (arg3 > res) res = arg3;
+    return res;
+}
+
+int a1 = 122222, b1 = 21, c1 = 39;
+int a2 = 12, b2 = 2311, c2 = 33;
+int a3 = 13, b3 = 23, c3 = 313;
+
+//int max1 = Max(a1,b1,c1);
+//int max2 = Max(a2,b2,c2);
+//int max3 = Max(a3,b3,c3);
+//int Max(max1, max2, max3)
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
