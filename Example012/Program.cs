c4f208c2938c01fa4267e556f4b6f8a10d6b8b75
Orlang00r <Orlangur00r@yandex.ru>
2022-04-25 16:08:19 +0400

    FOR

--- Example012/Program.cs
+++ Example012/Program.cs
@@ -1,4 +1,6 @@
-﻿// Вид 1
+﻿/*
+
+// Вид 1
 
 void Method1()
 {
@@ -54,3 +56,34 @@ string Method4(int count, string c)
 
 string res = Method4(10, "z");
 Console.WriteLine(res);
+
+
+string Method4(int count, string c)
+{
+
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + c;
+    }
+    return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+
+*/
+
+
+
